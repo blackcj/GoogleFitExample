c9d1f4ddcca48c9b28aca9582aa79d69c1bbb67a
Chris Black <blackcj2@gmail.com>
2015-05-01 22:01:13 -0500

    Added screen shot and third party library list.

---
+++ app/src/main/java/com/google/android/gms/fit/samples/basichistoryapi/WorkoutReport.java
@@ -0,0 +1,84 @@
+package com.google.android.gms.fit.samples.basichistoryapi;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by chris.black on 5/1/15.
+ */
+public class WorkoutReport {
+    Map<Integer,Workout> map =  new HashMap<>();
+
+
+    public void addWorkoutData(Workout workout) {
+
+        if(map.get(workout.type) == null) {
+            map.put(workout.type, workout);
+        }else {
+            Workout w = map.get(workout.type);
+            w.stepCount += workout.stepCount;
+            w.duration += workout.duration;
+        }
+    }
+
+    public Workout getWorkoutByType(int type) {
+        return map.get(type);
+    }
+
+    public String toString() {
+        String result = "";
+        Set keys = map.keySet();
+        for (Iterator i = keys.iterator(); i.hasNext();)
+        {
+            int key = (int) i.next();
+            Workout value = (Workout) map.get(key);
+            result += WorkoutTypes.getWorkOutById(value.type) + " steps: " + value.stepCount + "\n";
+            result += WorkoutTypes.getWorkOutById(value.type) + " duration: " + getDurationBreakdown(value.duration) + "\n";
+        }
+        return result;
+    }
+
+    private double miliToMinutes(long mili) {
+        return Math.floor(mili / 1000 / 60);
+    }
+
+    /**
+     * Convert a millisecond duration to a string format
+     *
+     * @param millis A duration to convert to a string form
+     * @return A string of the form "X Days Y Hours Z Minutes A Seconds".
+     */
+    public static String getDurationBreakdown(long millis)
+    {
+        if(millis < 0)
+        {
+            throw new IllegalArgumentException("Duration must be greater than zero!");
+        }
+
+        long days = TimeUnit.MILLISECONDS.toDays(millis);
+        millis -= TimeUnit.DAYS.toMillis(days);
+        long hours = TimeUnit.MILLISECONDS.toHours(millis);
+        millis -= TimeUnit.HOURS.toMillis(hours);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+        millis -= TimeUnit.MINUTES.toMillis(minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+        StringBuilder sb = new StringBuilder(64);
+        if(days > 0) {
+            sb.append(days);
+            sb.append(" Days ");
+        }
+        if(hours > 0) {
+            sb.append(hours);
+            sb.append(" Hours ");
+        }
+        sb.append(minutes);
+        sb.append(" Minutes ");
+
+        return(sb.toString());
+    }
+}
---
+++ app/src/main/java/com/google/android/gms/fit/samples/basichistoryapi/WorkoutTypes.java
@@ -0,0 +1,43 @@
+package com.google.android.gms.fit.samples.basichistoryapi;
+
+/**
+ * Created by chris.black on 5/1/15.
+ */
+public class WorkoutTypes {
+    public static String getWorkOutById(int id) {
+        String result = "Unknown";
+        switch (id) {
+            case 1:
+                result = "Biking";
+                break;
+            case 7:
+                result = "Walking";
+                break;
+            case 8:
+                result = "Running";
+                break;
+            case 97:
+                result = "Weightlifting";
+                break;
+            case 9:
+                result = "Aerobics";
+                break;
+            case 3:
+                result = "Still";
+                break;
+            case 80:
+                result = "Strength training";
+                break;
+            case 4:
+                result = "Unknown";
+                break;
+            case 0:
+                result = "In vehicle";
+                break;
+            default:
+                result = "ID: " + id + " not defined";
+                break;
+        }
+        return result;
+    }
+}
