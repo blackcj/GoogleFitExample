e554d5d96738fb25db938b05edf2f719f5800011
Chris Black <blackcj2@gmail.com>
2015-05-01 19:11:45 -0500

    Initial check in

---
+++ app/src/main/java/com/google/android/gms/fit/samples/basichistoryapi/MainActivity.java
@@ -0,0 +1,432 @@
+/*
+ * Copyright (C) 2014 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.gms.fit.samples.basichistoryapi;
+
+import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.fit.samples.common.logger.Log;
+import com.google.android.gms.fit.samples.common.logger.LogView;
+import com.google.android.gms.fit.samples.common.logger.LogWrapper;
+import com.google.android.gms.fit.samples.common.logger.MessageOnlyLogFilter;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataSource;
+import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataDeleteRequest;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResult;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This sample demonstrates how to use the History API of the Google Fit platform to insert data,
+ * query against existing data, and remove data. It also demonstrates how to authenticate
+ * a user with Google Play Services and how to properly represent data in a {@link DataSet}.
+ */
+public class MainActivity extends ActionBarActivity {
+    public static final String TAG = "BasicHistoryApi";
+    private static final int REQUEST_OAUTH = 1;
+    private static final String DATE_FORMAT = "yyyy.MM.dd HH:mm:ss";
+
+    /**
+     *  Track whether an authorization activity is stacking over the current activity, i.e. when
+     *  a known auth error is being resolved, such as showing the account chooser or presenting a
+     *  consent dialog. This avoids common duplications as might happen on screen rotations, etc.
+     */
+    private static final String AUTH_PENDING = "auth_state_pending";
+    private boolean authInProgress = false;
+    private boolean connected = false;
+    private GoogleApiClient mClient = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // This method sets up our custom logger, which will print all log messages to the device
+        // screen, as well as to adb logcat.
+        initializeLogging();
+
+        if (savedInstanceState != null) {
+            authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
+        }
+
+        buildFitnessClient();
+    }
+
+    /**
+     *  Build a {@link GoogleApiClient} that will authenticate the user and allow the application
+     *  to connect to Fitness APIs. The scopes included should match the scopes your app needs
+     *  (see documentation for details). Authentication will occasionally fail intentionally,
+     *  and in those cases, there will be a known resolution, which the OnConnectionFailedListener()
+     *  can address. Examples of this include the user never having signed in before, or
+     *  having multiple accounts on the device and needing to specify which account to use, etc.
+     */
+    private void buildFitnessClient() {
+        // Create the Google API Client
+        mClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.HISTORY_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                .addConnectionCallbacks(
+                        new GoogleApiClient.ConnectionCallbacks() {
+                            @Override
+                            public void onConnected(Bundle bundle) {
+                                Log.i(TAG, "Connected!!!");
+                                // Now you can make calls to the Fitness APIs.  What to do?
+                                // Look at some data!!
+                                connected = true;
+
+                            }
+
+                            @Override
+                            public void onConnectionSuspended(int i) {
+                                // If your connection to the sensor gets lost at some point,
+                                // you'll be able to determine the reason and react to it here.
+                                if (i == ConnectionCallbacks.CAUSE_NETWORK_LOST) {
+                                    Log.i(TAG, "Connection lost.  Cause: Network Lost.");
+                                } else if (i == ConnectionCallbacks.CAUSE_SERVICE_DISCONNECTED) {
+                                    Log.i(TAG, "Connection lost.  Reason: Service Disconnected");
+                                }
+                                connected = false;
+                            }
+                        }
+                )
+                .addOnConnectionFailedListener(
+                        new GoogleApiClient.OnConnectionFailedListener() {
+                            // Called whenever the API client fails to connect.
+                            @Override
+                            public void onConnectionFailed(ConnectionResult result) {
+                                Log.i(TAG, "Connection failed. Cause: " + result.toString());
+                                if (!result.hasResolution()) {
+                                    // Show the localized error dialog
+                                    GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(),
+                                            MainActivity.this, 0).show();
+                                    return;
+                                }
+                                // The failure has a resolution. Resolve it.
+                                // Called typically when the app is not yet authorized, and an
+                                // authorization dialog is displayed to the user.
+                                if (!authInProgress) {
+                                    try {
+                                        Log.i(TAG, "Attempting to resolve failed connection");
+                                        authInProgress = true;
+                                        result.startResolutionForResult(MainActivity.this,
+                                                REQUEST_OAUTH);
+                                    } catch (IntentSender.SendIntentException e) {
+                                        Log.e(TAG,
+                                                "Exception while starting resolution activity", e);
+                                    }
+                                }
+                            }
+                        }
+                )
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Connect to the Fitness API
+        Log.i(TAG, "Connecting...");
+        mClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mClient.isConnected()) {
+            mClient.disconnect();
+            connected = false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_OAUTH) {
+            authInProgress = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mClient.isConnecting() && !mClient.isConnected()) {
+                    mClient.connect();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(AUTH_PENDING, authInProgress);
+    }
+
+    int estimatedSteps = 0;
+    int walkingSteps = 0;
+    int runningSteps = 0;
+    int bikingSteps = 0;
+    int miscSteps = 0;
+
+    int walkingDuration = 0;
+    int runningDuration = 0;
+    int bikingDuration = 0;
+
+    /**
+     *  Create a {@link DataSet} to insert data into the History API, and
+     *  then create and execute a {@link DataReadRequest} to verify the insertion succeeded.
+     *  By using an {@link AsyncTask}, we can schedule synchronous calls, so that we can query for
+     *  data after confirming that our insert was successful. Using asynchronous calls and callbacks
+     *  would not guarantee that the insertion had concluded before the read request was made.
+     *  An example of an asynchronous call using a callback can be found in the example
+     *  on deleting data below.
+     */
+    private class ReadDataTask extends AsyncTask<Void, Void, Void> {
+        protected Void doInBackground(Void... params) {
+            estimatedSteps = 0;
+            walkingSteps = 0;
+            miscSteps = 0;
+            runningSteps = 0;
+            bikingSteps = 0;
+
+            walkingDuration = 0;
+            runningDuration = 0;
+            bikingDuration = 0;
+
+            // Setting a start and end date using a range of 1 week before this moment.
+            Calendar cal = Calendar.getInstance();
+            Long time = new Date().getTime();
+            Date now = new Date();//time - time % (24 * 60 * 60 * 1000));
+            cal.setTime(now);
+            cal.set(Calendar.HOUR_OF_DAY, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+            cal.add(Calendar.DAY_OF_YEAR, -1);
+            long endTime = cal.getTimeInMillis();
+            cal.add(Calendar.DAY_OF_YEAR, -1);
+            long startTime = cal.getTimeInMillis();
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+            Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
+            Log.i(TAG, "Range End: " + dateFormat.format(endTime));
+
+            // Estimated steps and duration by Activity
+            DataReadRequest activitySegmentRequest = queryActivitySegment(startTime, endTime);
+            DataReadResult dataReadResult = Fitness.HistoryApi.readData(mClient, activitySegmentRequest).await(1, TimeUnit.MINUTES);
+            parseActivityData(dataReadResult);
+            printActivityData();
+
+            // Estimated steps by bucket
+            DataReadRequest stepCountRequest = queryStepEstimate(startTime, endTime);
+            dataReadResult = Fitness.HistoryApi.readData(mClient, stepCountRequest).await(1, TimeUnit.MINUTES);
+            parseStepEstimateData(dataReadResult);
+            printEstimatedStepData();
+
+            return null;
+        }
+    }
+
+    private void printActivityData() {
+        Log.i(TAG, "Walking steps: " + walkingSteps);
+        Log.i(TAG, "Walking duration: " + miliToMinutes(walkingDuration) + " minutes");
+        Log.i(TAG, "Misc steps: " + miscSteps);
+        Log.i(TAG, "Biking steps: " + bikingSteps);
+        Log.i(TAG, "Biking duration: " + miliToMinutes(bikingDuration) + " minutes");
+        Log.i(TAG, "Running steps: " + runningSteps);
+        Log.i(TAG, "Running duration: " + miliToMinutes(runningDuration) + " minutes");
+    }
+
+    private void printEstimatedStepData() {
+        Log.i(TAG, "Estimated steps: " + estimatedSteps);
+    }
+
+    private double miliToMinutes(long mili) {
+        return Math.floor(mili / 1000 / 60);
+    }
+
+    /**
+     * GET data by ACTIVITY
+     *
+     */
+    private DataReadRequest queryActivitySegment(long startTime, long endTime) {
+        // [START build_read_data_request]
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .read(DataType.TYPE_ACTIVITY_SEGMENT)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+        // [END build_read_data_request]
+        return readRequest;
+    }
+
+    private void parseActivityData(DataReadResult dataReadResult) {
+        for (DataSet dataSet : dataReadResult.getDataSets()) {
+            parseDataSet(dataSet);
+        }
+    }
+
+    /**
+     * GET total estimated STEP_COUNT
+     *
+     */
+    private DataReadRequest queryStepEstimate(long startTime, long endTime) {
+        DataSource ESTIMATED_STEP_DELTAS = new DataSource.Builder()
+                .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+                .setType(DataSource.TYPE_DERIVED)
+                .setStreamName("estimated_steps")
+                .setAppPackageName("com.google.android.gms")
+                .build();
+
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(ESTIMATED_STEP_DELTAS, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+        return readRequest;
+    }
+
+    private void parseStepEstimateData(DataReadResult dataReadResult) {
+        for (Bucket bucket : dataReadResult.getBuckets()) {
+            for (DataSet dataSet : bucket.getDataSets()) {
+                estimatedSteps += parseDataSet(dataSet);
+            }
+        }
+    }
+
+    /**
+     * Return a {@link DataReadRequest} for all step count changes in the past week.
+     */
+    private DataReadRequest queryStepCount(long startTime, long endTime) {
+        // [START build_read_data_request]
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+        // [END build_read_data_request]
+
+        return readRequest;
+    }
+
+    // https://developers.google.com/fit/rest/v1/reference/activity-types
+
+    private void countStepDataByActivity(DataReadResult dataReadResult, int activity) {
+        for (Bucket bucket : dataReadResult.getBuckets()) {
+            for (DataSet dataSet : bucket.getDataSets()) {
+                if(activity == 7) {
+                    walkingSteps += parseDataSet(dataSet);
+                } else if(activity == 8) {
+                    runningSteps += parseDataSet(dataSet);
+                } else if(activity == 1) {
+                    bikingSteps += parseDataSet(dataSet);
+                }  else {
+                    miscSteps += parseDataSet(dataSet);
+                }
+            }
+        }
+    }
+
+    // [START parse_dataset]
+    private int parseDataSet(DataSet dataSet) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        int dataSteps = 0;
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            for(Field field : dp.getDataType().getFields()) {
+                if(dp.getDataType().getName().equals("com.google.step_count.delta") && dp.getValue(field).asInt() > 0) {
+                    dataSteps += dp.getValue(field).asInt();
+                }
+            }
+
+            for(Field field : dp.getDataType().getFields()) {
+                if(field.getName().equals("activity") && dp.getDataType().getName().equals("com.google.activity.segment")) {
+                    long startTime = dp.getStartTime(TimeUnit.MILLISECONDS);
+                    long endTime = dp.getEndTime(TimeUnit.MILLISECONDS);
+                    int activity = dp.getValue(field).asInt();
+                    if(activity == 7) {
+                        walkingDuration += endTime - startTime;
+                    } else if(activity == 8) {
+                        runningDuration += endTime - startTime;
+                    } else if(activity == 1) {
+                        bikingDuration += endTime - startTime;
+                    }
+                    //Log.i(TAG, "\tField: " + field.getName() + " Value: " + dp.getValue(field));
+                    DataReadRequest readRequest = queryStepCount(startTime, endTime);
+                    DataReadResult dataReadResult = Fitness.HistoryApi.readData(mClient, readRequest).await(1, TimeUnit.MINUTES);
+                    countStepDataByActivity(dataReadResult, activity);
+                }
+            }
+        }
+
+        return dataSteps;
+    }
+    // [END parse_dataset]
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_delete_data) {
+            if(connected) {
+                new ReadDataTask().execute();
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     *  Initialize a custom log class that outputs both to in-app targets and logcat.
+     */
+    private void initializeLogging() {
+        // Wraps Android's native log framework.
+        LogWrapper logWrapper = new LogWrapper();
+        // Using Log, front-end to the logging chain, emulates android.util.log method signatures.
+        Log.setLogNode(logWrapper);
+        // Filter strips out everything except the message text.
+        MessageOnlyLogFilter msgFilter = new MessageOnlyLogFilter();
+        logWrapper.setNext(msgFilter);
+        // On screen logging via a customized TextView.
+        LogView logView = (LogView) findViewById(R.id.sample_logview);
+        logView.setTextAppearance(this, R.style.Log);
+        logView.setBackgroundColor(Color.WHITE);
+        msgFilter.setNext(logView);
+        Log.i(TAG, "Ready");
+    }
+}
